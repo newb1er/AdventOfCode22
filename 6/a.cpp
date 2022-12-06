8fb129344f9b6bed73d912598b76ecbf9a9eae2c
newb1er <taiwanmichael429@gmail.com>
2022-12-07 01:01:08 +0800

    feat(6): part 1

---
+++ 6/a.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+
+const int N = 4;
+
+bool different_char(std::string& s, size_t begin, size_t end) {
+  for (size_t i = begin; i < end; ++i) {
+    auto pos = s.find(s.at(i), i + 1);
+    if (pos != std::string::npos && pos < end) return false;
+  }
+
+  return true;
+}
+
+int main() {
+  std::string str;
+
+  std::cin >> str;
+
+  for (size_t end = N; end <= str.size(); ++end) {
+    if (different_char(str, end - N, end)) {
+      std::cout << end << '\n';
+      break;
+    }
+  }
+
+  return 0;
+}
